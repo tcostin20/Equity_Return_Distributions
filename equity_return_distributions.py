3bc3587c67dae05bd987227c406ab50216a29ae1
tcostin20 <tcostin20@gmail.com>
2026-08-03 11:38:44 -0400

    Add docstring for equity return distributions analysis

---
+++ equity_return_distributions.py
@@ -0,0 +1,8 @@
+'''
+------------------------------------------------------------------------------
+Description: Analysis of equity return distributions for various asset classes
+Author: Thomas Costin (tcostin20@gmail.com)
+Date started: 8/3/2026
+Data source: TBU
+------------------------------------------------------------------------------
+'''
