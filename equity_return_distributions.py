8d712ac1c8de431e2bf749fbcdba8d8260f74800
tcostin20 <tcostin20@gmail.com>
2026-08-03 12:22:27 -0400

    Add test message for branch functionality

--- equity_return_distributions.py
+++ equity_return_distributions.py
@@ -7,3 +7,4 @@ Data source: TBU
 ------------------------------------------------------------------------------
 '''
 
+'This is a test to see if my branches are working correctly. I will be creating a new branch to test this out.'
