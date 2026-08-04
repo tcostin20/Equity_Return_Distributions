ccebef3160b4620f37f6008f430f18f35246ec1f
tcostin20 <tcostin20@gmail.com>
2026-08-03 20:27:49 -0400

    Implement data retrieval function and example usage for equity return distributions

--- equity_return_distributions.py
+++ equity_return_distributions.py
@@ -7,4 +7,28 @@ Data source: TBU
 ------------------------------------------------------------------------------
 '''
 
-'This is a test to see if my branches are working correctly. I will be creating a new branch to test this out.'
+import yfinance as yf
+import pandas as pd
+
+###########################################################
+def get_data(ticker:list) -> pd.DataFrame:
+    '''
+    Function that returns equity return data for each ticker the
+    user provides. The data is returned as a DataFrame.
+    
+    Parameters
+    ----------
+    ticker (list): A list of ticker symbols for which to retrieve equity return data.
+    
+    Returns
+    ----------
+    data (DataFrame): A DataFrame containing equity return distributions.
+    '''
+    pass
+
+###########################################################
+def main():
+    # Example usage
+    tickers = ['AAPL', 'MSFT', 'GOOGL']
+    data = get_data(tickers)
+    print(data)
